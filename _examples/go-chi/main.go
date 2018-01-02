8349e44e210ade28e2fa1ab737c6418754473892
Liam Stanley <me@liamstanley.io>
2018-01-02 16:08:47 -0500

    update examples

--- _examples/go-chi/main.go
+++ _examples/go-chi/main.go
@@ -1,30 +1,27 @@
 package main
 
 import (
+	"expvar"
 	_ "expvar"
 	"fmt"
 	"net/http"
 
 	"github.com/go-chi/chi"
-	"github.com/go-chi/chi/middleware"
 	"github.com/lrstanley/httpstat"
 	"github.com/lrstanley/httpstat/statgraph"
 )
 
 func main() {
-	stats := httpstat.New("", nil)
+	// This example includes usage of the statgraph sub-package!
+	stats := httpstat.New("", &httpstat.HistoryOptions{Enabled: true})
 	defer stats.Close()
 
 	r := chi.NewRouter()
 	r.Use(stats.Record)
-	r.Use(middleware.DefaultLogger)
 
-	r.Mount("/debug", middleware.Profiler())
+	r.Mount("/debug/vars", expvar.Handler())
 	r.Mount("/graphs*", http.StripPrefix("/graphs", statgraph.New(stats)))
-
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Hello Gopher")
-	})
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) { fmt.Fprintln(w, "Hello Gopher") })
 
 	http.ListenAndServe(":8080", r)
 }
--- _examples/stdlib/main.go
+++ _examples/stdlib/main.go
@@ -12,9 +12,7 @@ func main() {
 	stats := httpstat.New("", nil)
 	defer stats.Close()
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Hello Gopher")
-	})
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { fmt.Fprintln(w, "Hello Gopher") })
 
 	http.ListenAndServe(":8080", stats.Record(http.DefaultServeMux))
 }
